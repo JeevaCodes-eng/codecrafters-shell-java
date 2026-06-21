d8a17eb03f5ea5f39a89d7045dbbaa9624ac36ac
Jeeva J <jeevajanardhanan458@gmail.com>
2026-06-21 22:53:58 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,6 +18,20 @@ public class Main {
                 continue;
             }
 
+            if (input.startsWith("type ")) {
+                String command = input.substring(5);
+
+                if (command.equals("echo")
+                        || command.equals("exit")
+                        || command.equals("type")) {
+                    System.out.println(command + " is a shell builtin");
+                } else {
+                    System.out.println(command + ": not found");
+                }
+
+                continue;
+            }
+
             System.out.println(input + ": command not found");
         }
 
